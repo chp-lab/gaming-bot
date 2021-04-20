f30c3baddb8501f7bbd7c461adb7aa80c6b2498b
chatpeth.ke <chatpeth.ke@inet.co.th>
2021-04-20 17:26:48 +0700

    change setting

--- mysql_config.py
+++ mysql_config.py
@@ -5,8 +5,9 @@ class MysqlConfig:
     mysql_conf_file = ""
     def __init__(self):
         TAG = "mysql_config:"
-        print(TAG, "current dir=", pathlib.Path(__file__).resolve().parent)
-        self.mysql_conf_file = "./database_config.yaml"
+        my_dir = pathlib.Path(__file__).resolve().parent
+        print(TAG, "current dir=", my_dir)
+        self.mysql_conf_file = my_dir + "/database_config.yaml"
     def showData(self):
         print("Testing")
     def callDBConfig(self):
